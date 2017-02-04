2d9cd8f06a78309271cf9fc51c3e5c6fccf433f6
Justin Wong <justinwong12337@gmail.com>
2017-02-04 23:58:40 +0800

    Add new Contact abstract, super class

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,38 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Contact from which Person, Email and Address inherit 
+ */
+public abstract class Contact {
+	public final String value;
+	private boolean isPrivate;
+	
+	/**
+     * Validates given contact.
+     *
+     * @throws IllegalValueException if given address string is invalid.
+     */
+    public Contact(String contact, boolean isPrivate) {
+        String trimmedContact = contact.trim();
+        this.isPrivate = isPrivate;
+        // Assuming that we can't change the method declaration of Address, Email and Phone constructors
+        // then have the subclasses carry out field validation
+        this.value = trimmedContact;
+    }
+    
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+    
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+    
+    @Override
+    public String toString() {
+        return value;
+    }
+}
